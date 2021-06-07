364c8a5f65af9989ae20e446bcaa8614431a04ae
Documenter.jl <documenter@juliadocs.github.io>
2021-06-07 09:31:28 +0000

    build based on cf56fa3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SGtSNEpi","category":"page"},{"location":"#SGtSNEpi","page":"Home","title":"SGtSNEpi","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SGtSNEpi.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SGtSNEpi]","category":"page"}]
+}
